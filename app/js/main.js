fb5a9f7eb53591ecb84facee4ddf5bf30e8ed5bb
sonysmile999@gmail.com <sonysmile999@gmail.com>
2023-03-08 08:41:48 +0200

    style: add style navigation + hover effect

--- app/js/main.js
+++ app/js/main.js
@@ -155,8 +155,11 @@ __webpack_require__.r(__webpack_exports__);
   \*********************************/
 /***/ (() => {
 
-let a = 25;
-console.log(a);
+const logo = document.querySelector(".navigation-logo");
+const link = document.createElement("a");
+link.href = "google.com";
+link.appendChild(logo.children[0]);
+logo.appendChild(link);
 
 /***/ }),
 
--- src/js/components/ex.js
+++ src/js/components/ex.js
@@ -1,2 +1,5 @@
-let a = 25;
-console.log(a);
+const logo = document.querySelector(".navigation-logo");
+const link = document.createElement("a");
+link.href = "google.com";
+link.appendChild(logo.children[0]);
+logo.appendChild(link);
