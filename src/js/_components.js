f795845809f8fd0a62b414eefe99f707d20b9c02
sonysmile999@gmail.com <sonysmile999@gmail.com>
2023-03-16 21:01:33 +0200

    feat: Added stylization + functionality of sending a form to a Telegram bot.

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,4 +1,5 @@
 import ex from "./components/ex.js";
-import slider from "./components/slider"
-import services from "./components/services-tab"
+import slider from "./components/slider";
+import services from "./components/services-tab";
 import works from "./components/works.js";
+import form from "./components/form.js";
